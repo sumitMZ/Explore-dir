8e04391bb28578dbdcae8849b6244328ea093f54
Sumit <sumitMZ>
2021-05-07 21:11:15 +0530

    python module to list content of directory in tree format

---
+++ explore_dir.py
@@ -0,0 +1,19 @@
+import os
+
+def opm(path,bull) :
+    drs = os.listdir(path)
+    bull+="---"
+    if not drs :
+        return
+    else:
+        for i in drs:
+            if os.path.isfile(path+'/'+i) :
+                print(bull,'> File: ',(i))
+            elif os.path.isdir(path+'/'+i) :
+                print(bull,'> Dir: ',(i))
+                opm(path+'/'+i,bull)
+        print()
+dirs =('/'+"home")
+print("\n\t",dirs)
+opm(dirs,"=>")
+
